bf225eee97d0da019f29db0548a9b57458c5c5ae
matthew-holman <matthew.holman@metry.io>
2021-05-24 20:42:04 +0200

    remove cmake debug

--- cmake-build-debug/CMakeFiles/blink-upload.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/blink-upload"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/blink-upload.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
